ed4b07c8ce81637f51faf14c0cf00a437d10ada8
phxtho <phathopukwana@gmail.com>
2021-02-23 00:57:44 +0200

    WIP Trying to get pixel colour

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("hello world!");
+async function getMedia(constraints: MediaStreamConstraints) {
+  let stream = null;
+
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    let video = document.getElementById("video") as HTMLVideoElement;
+    video.srcObject = stream;
+    video.onloadedmetadata = (e) => video.play();
+    video.addEventListener("click", (e) => processVideoClick(e, video));
+    video.addEventListener("mousemove", (e) => {
+      let output = document.getElementById("mouse-pos");
+      if (output)
+        output.innerText = `clientX:${e.clientX} , clientY: ${e.clientX}
+        offsetX:${e.offsetX}, offsetY:${e.offsetY}`;
+    });
+  } catch (error) {
+    console.log(error.name + ": " + error.message);
+  }
+}
+
+function processVideoClick(e: MouseEvent, video: HTMLVideoElement) {
+  let canvas = document.createElement("canvas");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  let canvasCtx = canvas.getContext("2d");
+  canvasCtx?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+  document.getElementById("app")?.append(canvas);
+  let pixel = canvasCtx?.getImageData(e.offsetX, e.offsetX, 1, 1).data;
+
+  let output = document.getElementById("pixel-pos");
+  if (output)
+    output.innerText = `r:${pixel?.[0]} g:${pixel?.[1]} b:${pixel?.[2]} \noffsetX:${e.offsetX}, offsetY:${e.offsetY} \nvideoWidth:${video.videoWidth} videoHeight:${video.videoHeight}`;
+}
+
+window.onload = (e: Event) => getMedia({ video: true });
