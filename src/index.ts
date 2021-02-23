9bc7a18abd2f9aca03c6b53fab7eb0400cb95c58
phxtho <phathopukwana@gmail.com>
2021-02-23 20:40:31 +0200

    get correct pixel from canvas

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,7 @@ async function getMedia(constraints: MediaStreamConstraints) {
     video.srcObject = stream;
     video.onloadedmetadata = (e) => video.play();
     video.addEventListener("click", (e) => processVideoClick(e, video));
-    video.addEventListener("mousemove", (e) => {
-      let output = document.getElementById("mouse-pos");
-      if (output)
-        output.innerText = `clientX:${e.clientX} , clientY: ${e.clientX}
-        offsetX:${e.offsetX}, offsetY:${e.offsetY}`;
-    });
+    video.addEventListener("mousemove", (e) => processVideoClick(e, video));
   } catch (error) {
     console.log(error.name + ": " + error.message);
   }
@@ -24,12 +19,23 @@ function processVideoClick(e: MouseEvent, video: HTMLVideoElement) {
   canvas.height = video.videoHeight;
   let canvasCtx = canvas.getContext("2d");
   canvasCtx?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  document.getElementById("app")?.append(canvas);
-  let pixel = canvasCtx?.getImageData(e.offsetX, e.offsetX, 1, 1).data;
+  let pixel = canvasCtx?.getImageData(e.offsetX, e.offsetY, 1, 1).data;
+
+  paintAppBackground(pixel);
 
   let output = document.getElementById("pixel-pos");
-  if (output)
-    output.innerText = `r:${pixel?.[0]} g:${pixel?.[1]} b:${pixel?.[2]} \noffsetX:${e.offsetX}, offsetY:${e.offsetY} \nvideoWidth:${video.videoWidth} videoHeight:${video.videoHeight}`;
+  if (output) {
+    const [r, g, b, a] = pixel;
+    output.innerText = `r:${r} g:${g} b:${b} \noffsetX:${e.offsetX}, offsetY:${e.offsetY} \nvideoWidth:${video.videoWidth} videoHeight:${video.videoHeight}`;
+  }
 }
 
 window.onload = (e: Event) => getMedia({ video: true });
+
+function paintAppBackground(pixelData: Uint8ClampedArray | undefined) {
+  if (pixelData) {
+    let app = document.getElementById("app");
+    const [r, g, b, a] = pixelData;
+    app?.setAttribute("style", `background-color: rgba(${r},${g},${b},${a});`);
+  }
+}
