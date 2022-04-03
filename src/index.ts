941ecbcfb271fb5cc31f7fd0e46dd6eb7b90ff9e
phxtho <phathopukwana@gmail.com>
2022-04-03 19:45:51 +0200

    use bundler-less wasm-pack output for ntc-rs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { closest_colour, rgb_to_hsl } from "ntc-rs";
+import init,{ closest_colour, rgb_to_hsl } from "../node_modules/ntc-rs";
 
 function placeMouseBox(e: MouseEvent) {
   let mouseBox = document.getElementById("mouse-box");
@@ -26,43 +26,49 @@ async function getMedia(constraints: MediaStreamConstraints) {
 }
 
 function processVideoClick(e: MouseEvent, video: HTMLVideoElement) {
-  let canvas = document.createElement("canvas");
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  let canvasCtx = canvas.getContext("2d");
-  canvasCtx?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  const x = (video.videoWidth / video.clientWidth) * e.offsetX;
-  const y = (video.videoHeight / video.clientHeight) * e.offsetY;
-  let pixel = canvasCtx?.getImageData(x, y, 1, 1).data;
-
-  paintAppBackground(pixel);
-
-  let output = document.getElementById("pixel-pos");
-  if (output && pixel) {
-    const [r, g, b] = pixel;
-    const rgb = new Int32Array([r, g, b]);
-    const [h, s, l] = rgb_to_hsl(rgb);
-    output.innerText = `r:${r} g:${g} b:${b}\n h:${h} s:${s} l:${l}  \noffsetX:${e.offsetX}, offsetY:${e.offsetY} \nvideoWidth:${video.videoWidth} videoHeight:${video.videoHeight}`;
-    const approxColour = closest_colour(rgb);
-    if (approxColour) {
-      let lightness = "";
-      if (l > 0 && l < 255 / 3) {
-        lightness = "Dark ";
-      } else if (l > (2 * 255) / 3 && l <= 255) {
-        lightness = "Light ";
+  try {
+    let canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    let canvasCtx = canvas.getContext("2d");
+    canvasCtx?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    const x = (video.videoWidth / video.clientWidth) * e.offsetX;
+    const y = (video.videoHeight / video.clientHeight) * e.offsetY;
+    let pixel = canvasCtx?.getImageData(x, y, 1, 1).data;
+  
+    paintAppBackground(pixel);
+  
+    let output = document.getElementById("pixel-pos");
+    if (output && pixel) {
+      const [r, g, b] = pixel;
+      const rgb = new Int32Array([r, g, b]);
+      const [h, s, l] = rgb_to_hsl(rgb);
+      output.innerText = `r:${r} g:${g} b:${b}\n h:${h} s:${s} l:${l}  \noffsetX:${e.offsetX}, offsetY:${e.offsetY} \nvideoWidth:${video.videoWidth} videoHeight:${video.videoHeight}`;
+      const approxColour = closest_colour(rgb);
+      if (approxColour) {
+        let lightness = "";
+        if (l > 0 && l < 255 / 3) {
+          lightness = "Dark ";
+        } else if (l > (2 * 255) / 3 && l <= 255) {
+          lightness = "Light ";
+        }
+        lightness =
+          approxColour.shade.trim().toLowerCase() != "white" ||
+            approxColour.shade.trim().toLowerCase() != "black"
+            ? lightness
+            : "";
+        output.innerText += `\ncolour name: ${approxColour.name}\nshade: ${lightness + approxColour.shade
+          }`;
       }
-      lightness =
-        approxColour.shade.trim().toLowerCase() != "white" ||
-          approxColour.shade.trim().toLowerCase() != "black"
-          ? lightness
-          : "";
-      output.innerText += `\ncolour name: ${approxColour.name}\nshade: ${lightness + approxColour.shade
-        }`;
     }
+  }catch(e) {
+    console.error(e)
   }
+  
 }
 
-window.onload = () => {
+window.onload = async () => {
+  await init();
   getMedia({ video: true });
 };
 
